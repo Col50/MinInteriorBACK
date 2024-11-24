f99faa5048d8b692a1fc30aacdd47e26e95165fc
Juan David Hernández <juandhdez94@gmail.com>
2024-11-24 13:17:15 +0000

    formatted migration with prettier

--- src/migrations/1732307009733-CreateTableConfiguration.ts
+++ src/migrations/1732307009733-CreateTableConfiguration.ts
@@ -1,8 +1,6 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class CreateTableConfiguration1732307009733
-  implements MigrationInterface
-{
+export class CreateTableConfiguration1732307009733 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
             CREATE TABLE notification (
